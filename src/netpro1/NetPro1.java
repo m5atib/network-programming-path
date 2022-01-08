279610eacaee442d06775259fc4d9cb9000854d4
Muhaimen Khatib <64478989+m5atib@users.noreply.github.com>
2022-01-08 19:40:00 +0200

    first commit

--- src/netpro1/NetPro1.java
+++ src/netpro1/NetPro1.java
@@ -18,23 +18,44 @@ public class NetPro1 {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        generateCharacters(new FileOutputStream("src/TestingText/t1.txt"));
+        String outFile = "src/TestingText/writingData.txt";
+        String inFile = "src/TestingText/readingData.txt";
+
+        BufferedWriter bufout = new BufferedWriter(new FileWriter(outFile));
+        
+        try {
+            
+            readFile(inFile);
+            
+        }catch (Exception ex) {
+            
+        } finally {
+            
+        }
 
     }
-    
+
+    public static void readFile(String fileName) throws IOException {
+        BufferedReader bufin = new BufferedReader(new FileReader(fileName));
+        String line = "";
+        while ((line = bufin.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
     public static void generateCharacters(OutputStream out) throws Exception {
         int firstPrintableCharacter = 33;
         int numberOfPrintableCharacters = 94;
         int numberOfCharactersPerLine = 72;
         int start = firstPrintableCharacter;
-        byte s1[] = {97,98,99};
+        byte s1[] = {97, 98, 99};
         //the same as (char)97
         /* infinite loop */
         for (int i = start; i < start + numberOfCharactersPerLine; i++) {
             out.write(s1);
-            //out.write('\r'); // carriage return
+            out.write('\r'); // carriage return
             out.write('\n'); // linefeed
-            
+
         }
     }
 }
